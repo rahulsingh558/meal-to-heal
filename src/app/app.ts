beab5b56b256ea72dd4c10c204fe65db6a7ca444
Rahul Singh <meyok@Rahuls-Mac-mini.local>
2025-12-20 21:25:42 +0530

    Removed the header and footer from admin page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { filter } from 'rxjs/operators';
+
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 
@@ -15,4 +17,22 @@ import { FooterComponent } from './layout/footer/footer.component';
   ],
   templateUrl: './app.html',
 })
-export class App {}
+export class App {
+  isBrowser = false;
+  showLayout = true;
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) platformId: Object
+  ) {
+    this.isBrowser = isPlatformBrowser(platformId);
+
+    if (this.isBrowser) {
+      this.router.events
+        .pipe(filter(event => event instanceof NavigationEnd))
+        .subscribe((event: NavigationEnd) => {
+          this.showLayout = !event.urlAfterRedirects.startsWith('/admin');
+        });
+    }
+  }
+}
